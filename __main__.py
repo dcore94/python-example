86fb9f67bb5128c4a370ba351849ad7a7a7deb4f
dcore <dcore94@users.noreply.github.com>
2020-09-07 15:27:26 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,4 +1,10 @@
+from jinja2 import Template;
 import configparser;
+
 conf = configparser.ConfigParser()
 conf.read("config.cfg")
-print("Hello " + conf["default"]["name"])
+
+t = Template("Hello {{ who }}!")
+s = t.render(who=conf["default"]["name"])
+print(s)
+return s
