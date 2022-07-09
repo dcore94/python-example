517a5aa120bbeffa5005f4885b61b96ab012e255
dcore94 <m.lettere@gmail.com>
2022-07-09 11:28:16 +0200

    added file generation

--- __main__.py
+++ __main__.py
@@ -6,4 +6,16 @@ conf.read("config.cfg")
 
 t = Template("Ciao {{ who }}!")
 s = t.render(who=conf["default"]["name"])
+
+f = open("file.txt", "a")
+f.write(s + "\n")
+
+f = open("file.xml", "w")
+f.write("<text>" + s + "</text>")
+
+f = open("file.json", "w")
+f.write('{ "text" : "' + s + '"}')
+
 print(s)
+
+
